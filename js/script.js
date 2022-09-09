324a036de1cdaca29a24c6a6432a360cb9ea81a8
Isa Sulaiman <isasulaimanisa@gmail.com>
2022-09-09 03:16:47 +0100

    added event listener on input function to check input validity and display/hide error paragraph

--- js/script.js
+++ js/script.js
@@ -12,23 +12,67 @@ function inputName(){
 function inputNumber(){
     const cardNumberInput = document.getElementById("card-number").value;
     let numbers=cardNumberInput.match(/.{0,4}/g);
-    console.log(numbers.join(' '));
     document.getElementById("numberholder").innerHTML = numbers.join(' ');
+    const cardNumber = document.getElementById("card-number");
+    cardNumber.addEventListener('change', (e) => {
+        const isValid = e.target.checkValidity();
+        e.target.setAttribute('aria-invalid', !isValid);
+        if(!isValid){
+            document.getElementById("number-error").classList.remove("blank");
+        }else if(!document.getElementById("number-error").classList.contains("blank")){
+            document.getElementById("number-error").classList.add("blank");
+        }
+      });
 }
 
+
 function inputMM(){
+    // Display input on card
     const cardexpirymonthInput = document.getElementById("mm").value;
     document.getElementById("month-holder").innerHTML = cardexpirymonthInput;
+    // validate card details
+    const cardMonth = document.getElementById("mm");
+    cardMonth.addEventListener('change', (e) => {
+        const isValid = e.target.checkValidity();
+        e.target.setAttribute('aria-invalid', !isValid);
+        if(!isValid){
+            document.getElementById("expiry-error").classList.remove("blank");
+        }else if(!document.getElementById("expiry-error").classList.contains("blank")){
+            document.getElementById("expiry-error").classList.add("blank");
+        }
+    });
 }
 
 function inputYY(){
     const cardexpiryyearInput = document.getElementById("yy").value;
     document.getElementById("year-holder").innerHTML = cardexpiryyearInput;
+
+    const cardYear = document.getElementById("yy");
+    cardYear.addEventListener('change', (e) => {
+        const isValid = e.target.checkValidity();
+        e.target.setAttribute('aria-invalid', !isValid);
+        if(!isValid){
+            document.getElementById("expiry-error").classList.remove("blank");
+        }else if(!document.getElementById("expiry-error").classList.contains("blank")){
+            document.getElementById("expiry-error").classList.add("blank");
+        }
+    });
 }
 
 function inputCvc(){
     const cardcvcInput = document.getElementById("cvc").value;
     document.getElementById("cvcholder").innerHTML = cardcvcInput;
+
+    const cardCvc = document.getElementById("cvc");
+    cardCvc.addEventListener('change', (e) => {
+        const isValid = e.target.checkValidity();
+        e.target.setAttribute('aria-invalid', !isValid);
+        if(!isValid){
+            document.getElementById("cvc-error").classList.remove("blank");
+        }else if(!document.getElementById("cvc-error").classList.contains("blank")){
+            document.getElementById("cvc-error").classList.add("blank");
+        }
+    });
 }
 
 
